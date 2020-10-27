129b4c3c84e99b95fa223318a884bc4094f79459
nstaehel <73179155+nstaehel@users.noreply.github.com>
2020-10-27 11:35:42 +0100

    Ajout de la classe GuessMachine

--- guess_my_number/guess_my_number.py
+++ guess_my_number/guess_my_number.py
@@ -11,21 +11,52 @@ import random
 MIN = 0
 MAX = 1000
 
+class GuessMachine():
+    '''
+    I have a number in mind,
+    you have to guess it !
+    
+    + self._number_to_guess is generated during creation of the object
+    + Use 'guess(num)' method to make a guess
+    + I'll count the number of attempt in self.number_of_attempt
+    '''
+    def __init__(self):
+        self._number_to_guess = random.randint(MIN,MAX)
+        self.number_of_attempt = 0
+    
+    def guess(self,num):
+        '''
+        Attempt to find the right number
+        it returns 'too high', 'too low', 'found'
+
+        '''
+        self.number_of_attempt+=1
+        if num < self._number_to_guess:
+            return 'too low'
+        elif num > self._number_to_guess:
+            return 'too high'
+        else:
+            return 'found'
+    
+
+
 if __name__ == '__main__' :
-    number_to_guess=random.randint(MIN, MAX)
-    print('Hey ! Try to guess a number between %d and %d' %(MIN,MAX))
+    guess_machine = GuessMachine()
+    print('Hey! Try to guess a number between %d and %d' %(MIN,MAX))
     
     while True:
-        user_input=input('Your guess ?')
+        user_input = input('Your guess ?')
         try:
             user_attempt=int(user_input)
-            if user_attempt == number_to_guess:
-                print('Fantastic ! You could find the number I had in mind')
+            result = guess_machine.guess(user_attempt)
+            if result == 'find':
+                print(
+                    'Fantastic !You could find '
+                    'the number I had in mind'
+                    'in %d attempt' %guess_machine.number_of_attempt)
                 break
-            elif user_attempt < number_to_guess:
-                print('Too low')
             else:
-                print('Too high')
+                print(result)
         except ValueError:
             print('You joker... That was not an integer!')
                 
